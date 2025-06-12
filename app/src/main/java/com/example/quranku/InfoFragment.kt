a170e3f416d0265665181d1946df89a213662c3b
althof-z <althof.zpv@gmail.com>
2025-06-12 20:48:37 +0700

    UI: create Info Fragment base & create app Info

--- app/src/main/java/com/example/quranku/InfoFragment.kt
+++ app/src/main/java/com/example/quranku/InfoFragment.kt
@@ -5,21 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class InfoFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         return inflater.inflate(R.layout.fragment_info, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val viewPager = view.findViewById<ViewPager2>(R.id.view_pager_info)
+        val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout_info)
 
+        val adapter = InfoPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Informasi"
+                1 -> "Tutorial"
+                2 -> "Tajwid"
+                else -> null
+            }
+        }.attach()
+    }
 }
