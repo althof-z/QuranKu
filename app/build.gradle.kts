a84ed4210516827d79222ff3ffc472be4dee6ef4
althof-z <althof2131057@itpln.ac.id>
2025-11-05 18:08:49 +0700

    etc: add firebase file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,6 +50,10 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.audiovisualizer)
     implementation ("com.google.android.material:material:1.11.0")
+
+    //Firebase
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
     
     // Networking
     implementation(libs.retrofit.v290)
